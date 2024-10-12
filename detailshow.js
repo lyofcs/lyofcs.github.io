15df0fe10a493fcb816d0617c21687d7e5f5ddd5
lyofcs <lyofcs@sina.com>
2024-10-12 19:58:01 +0800

    Add files via upload

--- detailshow.js
+++ detailshow.js
@@ -4,14 +4,34 @@ function getQueryParam(param) {
 }
 
 var rank = getQueryParam('rank'); // 获取rank参数
-var storedString = localStorage.getItem("members");
+/*var storedString = localStorage.getItem("members");
+var jsonData = JSON.parse(storedString);*/
 
-var jsonData = JSON.parse(storedString);
+var data;
 
-console.log(jsonData);
+fetch('data.json')
+    .then(response => {
+        if (response.ok) {
+            return response.json(); // 将响应转换为JSON
+        }
+        throw new Error('Network response was not ok.');
+    })
+    .then(jsonData => {
+        data = jsonData;
+        console.log(data);
+        console.log(data.members[rank].tfaName);
+        console.log(data.members[rank].historyMMR);
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
+
+
+
+/*console.log(jsonData);
 
 console.log(jsonData.members[rank].tfaName);
-console.log(jsonData.members[rank].historyMMR);
+console.log(jsonData.members[rank].historyMMR);*/
 //console.log(data.members[rank].tfaName);
 
 //console.log(data.members[rank].historyMMR);
