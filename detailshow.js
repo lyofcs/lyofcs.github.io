d49fe68177821409cdfa21fbdc354f44ae658804
lyofcs <lyofcs@sina.com>
2024-10-17 18:38:47 +0800

    Update detailshow.js

--- detailshow.js
+++ detailshow.js
@@ -99,8 +99,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 var options = {
                     title: 'Company Performance',
-                    hAxis: { title: 'Year', titleTextStyle: { color: 'red' } },
-                    vAxis: { title: 'Performance', titleTextStyle: { color: 'red' } },
+                    hAxis: {
+                        title: '赛事', titleTextStyle: {
+                            color: 'red'
+                        },
+                        textStyle: {
+                            fontSize: 10,
+                        }
+                    },
+                    vAxis: { title: '积分', titleTextStyle: { color: 'red' } },
                     tooltip: {
                         text: 'X: %{x}, Y: %{y}',
                         trigger: 'both',
@@ -108,6 +115,25 @@ document.addEventListener('DOMContentLoaded', function () {
                     },
                     width: '100%',
                     height: '100%',
+
+                    animation: {
+                        onComplete: function () {
+                            var chartInstance = this.chart,
+                                ctx = chartInstance.ctx;
+
+                            ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                            ctx.textAlign = 'center';
+                            ctx.textBaseline = 'bottom';
+
+                            this.data.datasets.forEach(function (dataset, i) {
+                                var meta = chartInstance.controller.getDatasetMeta(i);
+                                meta.data.forEach(function (bar, index) {
+                                    var data = dataset.data[index];
+                                    ctx.fillText(data, bar._model.x, bar._model.y - 5);
+                                });
+                            });
+                        }
+                    }
                 };
 
                 var chart = new google.visualization.LineChart(document.getElementById('donutchart'));
